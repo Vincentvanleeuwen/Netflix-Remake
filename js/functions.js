fa2fa39d14e3a0a5d9f14c77f4c707aa90f38a74
shoockzy <shoockz@hotmail.com>
2019-09-24 12:07:42 +0200

    version 3

--- js/functions.js
+++ js/functions.js
@@ -3,32 +3,54 @@ let next = document.getElementById("new-next");
 let sectionOne = document.getElementById("section1");
 
 next.addEventListener("click", () => {
-  sectionOne.getElementsByClassName("category")[0].style.left = "-1375px";
-  sectionOne.getElementsByClassName("category-next")[0].style.display = "grid";
-  document.getElementsByClassName("show")[0].style.width = "200vw";
+    sectionOne.getElementsByClassName("category")[0].style.left = "-1375px";
+    sectionOne.getElementsByClassName("category-next")[0].style.display = "grid";
+    document.getElementsByClassName("show")[0].style.width = "200vw";
 
 
-  setTimeout(() => {
-    sectionOne.getElementsByClassName("category")[0].style.display = "none";
-    sectionOne.getElementsByClassName("category-next")[0].style.left = "0";
-    document.getElementsByClassName("show")[0].style.width = "100vw";
+    setTimeout(() => {
+        sectionOne.getElementsByClassName("category")[0].style.display = "none";
+        sectionOne.getElementsByClassName("category-next")[0].style.left = "0";
+        document.getElementsByClassName("show")[0].style.width = "100vw";
 
-  }, 500);
-
-
+    }, 500);
 
 
 });
 
 prev.addEventListener("click", () => {
-  sectionOne.getElementsByClassName("category")[0].style.display = "grid";
-  sectionOne.getElementsByClassName("category-next")[0].style.left = "1375px";
-  document.getElementsByClassName("show")[0].style.width = "200vw";
+    sectionOne.getElementsByClassName("category")[0].style.display = "grid";
+    sectionOne.getElementsByClassName("category-next")[0].style.left = "1375px";
+    document.getElementsByClassName("show")[0].style.width = "200vw";
 
-  setTimeout(() => {
-    sectionOne.getElementsByClassName("category-next")[0].style.display = "none";
-    sectionOne.getElementsByClassName("category")[0].style.left = "0";
-    document.getElementsByClassName("show")[0].style.width = "100vw";
-  }, 100);
+    setTimeout(() => {
+        sectionOne.getElementsByClassName("category-next")[0].style.display = "none";
+        sectionOne.getElementsByClassName("category")[0].style.left = "0";
+        document.getElementsByClassName("show")[0].style.width = "100vw";
+    }, 100);
 
 });
+
+function prevAll(element) {
+    let result = [];
+
+    while (element === element.previousElementSibling)
+        result.push(element);
+    return result;
+}
+document.ready = () => {
+    let show = document.getElementsByClassName('.small-movie');
+
+    show.addEventListener('mouseenter', function () {
+        // this.nextAll().classList.add('has-positive-translate');
+        this.prevAll().classList.add('has-negative-translate');
+
+    }).addEventListener('mouseleave', function () {
+        removeHasClasses();
+    });
+
+    function removeHasClasses() {
+        show.classList.remove('has-negative-translate has-positive-translate');
+    }
+
+};
